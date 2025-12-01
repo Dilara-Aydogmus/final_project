d385df02cb463d3d766934ca91efc8149efbf793
Dilara Aydoğmuş <dilaraydgms@gmail.com>
2025-12-01 18:10:31 +0300

    application.properties eklendi

---
+++ src/main/java/com/example/demo/DilaraaydogmusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilaraaydogmusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DilaraaydogmusApplication.class, args);
+	}
+
+}
